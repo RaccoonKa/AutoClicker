6ab29b1e47429cd7cf42a712b274bf2233b33985
Svetozar <144272966+RaccoonKa@users.noreply.github.com>
2025-07-19 19:22:32 +0200

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-from app import AutoClickerApp
-import tkinter as tk
-
-if __name__ == "__main__":
-    root = tk.Tk()
-    root.iconbitmap("assets/icon.ico")
-    app = AutoClickerApp(root)
-    root.mainloop()
